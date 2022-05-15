d8801c9a8edddc377e2af884dc0e67c0bbb89661
Conor949 <20090029@mail.wit.ie>
2022-05-15 22:06:54 +0100

    Week 3 Exercises done

---
+++ tasky/src/components/Form.js
@@ -0,0 +1,81 @@
+import React from "react";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+
+const AddTaskForm = (props) => {
+
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& .MuiOutlinedInput-root': { m: 1, width: '30ch' },
+      }}
+      onSubmit={props.submit}
+    >
+      <div>
+        <TextField
+          required
+          id="outlined-required"
+          name="title"
+          label="Task Title"
+          InputLabelProps={{ shrink: true }}
+          onChange={(event) => props.change(event)}
+        />
+      </div>
+      <div>
+        <TextField
+          required
+          name="deadline"
+          label="Deadline"
+          InputLabelProps={{ shrink: true }}
+          type="date"
+          onChange={(event) => props.change(event)}
+        />
+      </div>
+      <div>
+        <TextField
+          onChange={(event) => props.change(event)}
+          select 
+          required
+          label="Priority"
+          name="priority"
+          value={props.priority}
+          InputLabelProps={{ shrink: true }}
+        >
+          <MenuItem key="1" value="low">Low</MenuItem>
+          <MenuItem key="2" value="medium">Medium</MenuItem>
+          <MenuItem key="3" value="high">High</MenuItem>
+        </TextField>
+      </div>
+      <div>
+        <TextField
+          name="description"
+          id="outlined-multiline-static"
+          label="Task Details"
+          InputLabelProps={{ shrink: true }}
+          multiline
+          rows={4}
+          onChange={(event) => props.change(event)}
+        />
+      </div>
+      <div>
+        <Button 
+          type="submit"
+          variant="contained"
+          color="primary"
+          sx={{
+            m: 1,
+            p: 1,
+            width: '95%'
+          }}
+        >
+          Add Task
+        </Button>
+      </div>
+    </Box>
+  )
+};
+
+export default AddTaskForm;
